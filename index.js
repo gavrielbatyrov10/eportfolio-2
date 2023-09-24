108676521bae27d0ad555c9402dfe4adfce81d01
gavriel <gbatyrov10@gmail.com>
2023-09-24 13:34:34 -0400

    u

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 let isModalOpen = false;
 let contrastToggle = false;
-const scaleFactor = 1/20;
+const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
   const shapes = document.querySelectorAll(".shape");
-  const x = event.clientX  * scaleFactor;
-  const y = event.clientY  * scaleFactor;
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
   console.log(x, y);
-
 
   for (let i = 0; i < shapes.length; ++i) {
     const isOdd = i % 2 !== 0;
     const boolInt = isOdd ? -1 : 1;
-    // Added rotate after tutorial
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${
+      y * boolInt
+    }px) rotate(${x * boolInt * 10}deg)`;
   }
 }
 
